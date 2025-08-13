94b0979f495ecbeca0812a6d6dc45cc443c33408
Khushipawar37 <khushipawar987@gmail.com>
2025-08-14 00:14:37 +0530

    2 chapters done

---
+++ basics.py
@@ -0,0 +1,23 @@
+name = "khushi"
+a = 5
+b = 3
+
+print(a*b)
+print(name)
+
+d = (5==5)
+e = (5 != 5)
+
+print(d,e)
+
+print(type(d))
+print(type(name))
+
+flo = 32.3
+print(type(flo))
+
+flIn = int(flo)
+print(type(flIn))
+
+namee = input("Enter your name : ")
+print("Hi, " + namee)
