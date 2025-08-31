3cba8f49ce459f31e2f89de61df400af127d5a03
Khushipawar37 <khushipawar987@gmail.com>
2025-09-01 01:21:29 +0530

    Game created

--- perfect_guess.py
+++ perfect_guess.py
@@ -3,14 +3,14 @@ import random
 n = random.randint(1,100)
 
 a = -1
-guesses = 0
+guesses = 1
 
 while(a != n):
-    guesses += 1
     a = int(input("Guess the number: "))
     if(a > n):
         print("Lower number")
-    else:
+    elif(a < n):
         print("Higher number please")
+        guesses += 1
 
 print(f"You guessed {n} after {guesses} guesses")
